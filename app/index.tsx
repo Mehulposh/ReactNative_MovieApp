c2d298f772dd32f4ce3962010baf33b989a26f06
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-06-15 15:04:31 +0530

    added extended colors in tailwind.config

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 justify-center items-center bg-primary "
+    >
+      <Text className="text-6xl text-accent">Welcome to my movie app!</Text>
+    </View>
+  );
+}
